167f2688f0fae3ed496305bbfaf57ff912b35769
David Cordova <d.mcordova87@gmail.com>
2018-12-27 15:54:46 -0700

    initial commit with code foundation

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
